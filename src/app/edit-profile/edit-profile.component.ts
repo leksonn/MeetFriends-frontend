1c33937f654e67e574e25ea01413c0633058ba58
leksonn <lejla.celosmanovic@stu.ssst.edu.ba>
2024-04-01 15:25:40 +0200

    Added Edit Profile page

---
+++ src/app/edit-profile/edit-profile.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrl: './edit-profile.component.css'
+})
+export class EditProfileComponent {
+
+  form: FormGroup;
+
+  constructor(formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      name: ['Lejla Celosmanovic'],
+      username: ['leksonn'],
+      bio: ['Hi! Im 21 years old, and i have a dog!']
+    })
+  }
+
+  submitForm() {
+    console.log(this.form.value);
+  }
+}
