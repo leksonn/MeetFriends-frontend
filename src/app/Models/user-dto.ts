f9ffd6c0c2bfebd2885cc37540788a3766f062b2
dzani <dzanieterle531@gmail.com>
2024-05-24 20:27:46 +0200

    Connected frontend to DB, login works

---
+++ src/app/Models/user-dto.ts
@@ -0,0 +1,7 @@
+// src/app/models/user-dto.ts
+export interface UserDTO {
+    id: number;
+    username: string;
+    password: string;
+    email: string;
+}
--- src/app/sevices/login.service.ts
+++ src/app/sevices/login.service.ts
@@ -1,20 +1,30 @@
-import {HttpClient} from "@angular/common/http";
-import {Injectable, numberAttribute} from "@angular/core";
+// src/app/services/login.service.ts
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { UserDTO } from "../models/user-dto";
 
-@Injectable()
-export class LoginService{
-    Userarray:string[]=["dzanof","lekson","hodzo","undefined"];
-    constructor(){
+@Injectable({
+    providedIn: 'root'
+})
+export class LoginService {
+    private apiUrl = 'http://localhost:8080/api/user';
 
+    constructor(private http: HttpClient) { }
+
+    login(credentials: { username: string; password: string }): Observable<UserDTO> {
+        return this.http.get<UserDTO>(`${this.apiUrl}/login/${credentials.username}`);
     }
-    public getLoginInfo(test:string):boolean{
-        let i;
-        for (i=0;i<this.Userarray.length;i+=1){
-            console.log(i);
-            if (this.Userarray.at(i) == test){
-                return true
-            }
-        }
-        return  false
+
+    signup(user: { username: string; password: string; email: string }): Observable<UserDTO> {
+        return this.http.post<UserDTO>(`${this.apiUrl}/add`, user);
+    }
+
+    addFriend(userId: number, friendId: number): Observable<UserDTO> {
+        return this.http.put<UserDTO>(`${this.apiUrl}/${userId}/addFriend/${friendId}`, {});
+    }
+
+    removeFriend(userId: number, friendId: number): Observable<UserDTO> {
+        return this.http.put<UserDTO>(`${this.apiUrl}/${userId}/removeFriend/${friendId}`, {});
     }
 }
